88701dd681bb8799e9031af79ff2bf5af7736ee6
edwinmuriithi <kabuimuriithi@gmail.com>
2023-03-11 00:02:41 +0300

    feat(RecyclerView): Implement recycler view and pull to refresh feature

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,60 @@
+ package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+
+import com.example.recyclerview.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+ public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    RecyclerView recyclerView;
+    RecyclerAdapter recyclerAdapter;
+    List<String> moviesList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        moviesList = new ArrayList<>();
+        recyclerAdapter = new RecyclerAdapter(moviesList);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setAdapter(recyclerAdapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
+        binding.recyclerView.addItemDecoration(dividerItemDecoration);
+
+        moviesList.add("1. Iron Man (2008)");
+        moviesList.add("2. The Incredible Hulk (2008)");
+        moviesList.add("3. Iron Man 2 (2010)");
+        moviesList.add("4. Thor (2011)");
+        moviesList.add("5. Captain America: The First Avenger (2011)");
+        moviesList.add("6. The Avengers (2012)");
+        moviesList.add("7. Iron Man 3 (2013)");
+        moviesList.add("8. Thor: The Dark World (2013)");
+        moviesList.add("9. Captain America: The Winter Soldier (2014)");
+        moviesList.add("10. Guardians of the Galaxy (2014)");
+
+        binding.swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                moviesList.add("11. Avengers: Age of Ultron");
+                moviesList.add("12. Ant-man");
+                moviesList.add("13. Captain America: Civil War");
+                moviesList.add("14. Doctor Strange");
+                recyclerAdapter.notifyDataSetChanged();
+                binding.swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+    }
+}
